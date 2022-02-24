d8826e3290802129e2d02c5a84857d9c333b2144
RomainFelicite <romainfelicite32@gmail.com>
2022-02-24 14:03:01 +0400

    Fin lvl03 suite random

--- day_01/starter_day01_random_L03/exercicesAleatoires.js
+++ day_01/starter_day01_random_L03/exercicesAleatoires.js
@@ -17,3 +17,32 @@ function addZero(i) {
 var date_entiere1 =
   annee + "-" + mois + "-" + jour + " " + heure + ":" + minutes;
 console.log(date_entiere1);
+
+// 2 // ---
+let text1 = prompt("Saisir un mois de l'année");
+var string_vide = "";
+let expr_mois = text1.toLowerCase();
+if (expr_mois != string_vide) {
+  switch (expr_mois) {
+    case "janvier":
+    case " mars":
+    case "mai":
+    case " juilet":
+    case "aout":
+    case " septembre":
+    case "décembre":
+      console.log(expr_mois + " a 31 jours.");
+      break;
+    case "avril":
+    case "juin":
+    case "septembre":
+    case "novembre":
+      console.log(expr_mois + " a 30 jours.");
+      break;
+    case "fevrier":
+      console.log(expr_mois + " a 28 jours.");
+      break;
+    default:
+      console.log("Désolé ce mois n'existe pas !!!" + expr_mois + ".");
+  }
+}
